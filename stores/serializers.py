cf6e79b9e7a7fc94efa7b945d11703f3261f7380
Hojun Lee <akk808@snu.ac.kr>
2025-01-12 03:34:55 +0900

    feat: 각종 스토어 목록 보여주는 API 수정

--- stores/serializers.py
+++ stores/serializers.py
@@ -1,9 +1,50 @@
+from django.db.models import F
 from rest_framework import serializers
 
-from stores.models import Store
+from stores.models import Store, Prepay, UserPrepayRelease
 
 
-class StoreSerializer(serializers.ModelSerializer):
+class PrepaySerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Prepay
+        fields = ['username', 'credit']
+
+    def get_username(self, obj: Prepay):
+        return obj.user.username
+
+
+class PublicStoreSerializer(serializers.ModelSerializer):
+    public_prepay = serializers.SerializerMethodField()
+
     class Meta:
         model = Store
         fields = '__all__'
+
+    def get_public_prepay(self, obj: Store):
+        return obj.prepay_set.filter(type=Prepay.PrepayType.PUBLIC).annotate(username=F("user__username")).values('id', 'username', 'credit')
+
+
+class PrivateStoreSerializer(serializers.ModelSerializer):
+    private_prepay = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Store
+        fields = '__all__'
+
+    def get_private_prepay(self, obj: Store):
+        return obj.prepay_set.filter(type=Prepay.PrepayType.PRIVATE).annotate(username=F("user__username")).values('id', 'username', 'credit')
+
+
+class MyStoreSerializer(serializers.ModelSerializer):
+    my_prepay = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Store
+        fields = '__all__'
+
+    def get_my_prepay(self, obj: Store):
+        user = self.context['request'].user
+        my_prepay_ids = UserPrepayRelease.objects.filter(user=user).values_list('prepay_id', flat=True)
+        return Prepay.objects.filter(id__in=my_prepay_ids, store=obj).annotate(username=F("user__username")).values('id', 'username', 'credit')
--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import authenticate
 from rest_framework import generics, status
 from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
